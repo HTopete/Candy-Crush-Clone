6bd02bc6fd1e0c493ccd03c3252659160f51c15b
rtopete_x <hector.topete@sngular.com>
2024-03-16 02:46:09 -0600

    Se ajusta lógica para generar random al inicio

--- src/commonMain/kotlin/j4k/candycrush/level/LevelFactory.kt
+++ src/commonMain/kotlin/j4k/candycrush/level/LevelFactory.kt
@@ -5,24 +5,49 @@ import j4k.candycrush.model.Level.TileObjective
 
 class LevelFactory {
 
+    fun isPartOfThreeInARow(board: List<List<Tile>>, row: Int, col: Int): Boolean {
+        val candy = board[row][col]
+        val rowMatches = listOf(-2, -1, 0, 1, 2).count { board.getOrNull(row + it)?.getOrNull(col) == candy } >= 3
+        val colMatches = listOf(-2, -1, 0, 1, 2).count { board.getOrNull(row)?.getOrNull(col + it) == candy } >= 3
+        return rowMatches || colMatches
+    }
+    fun generateValidRandomBoard(): List<List<Tile>> {
+        val board = MutableList(9) { MutableList(5) { Tile.A } }  // Inicializa el tablero con cualquier caramelo
+        for (row in 0 until 9) {
+            for (col in 0 until 5) {
+                var candy: Tile
+                do {
+                    candy = Tile.randomTile()
+                    board[row][col] = candy
+                } while (isPartOfThreeInARow(board, row, col))
+            }
+        }
+        return board
+    }
+
+    fun boardToString(board: List<List<Tile>>): String {
+        return buildString {
+            for (row in board) {
+                append("[")
+                for ((index, tile) in row.withIndex()) {
+                    append(tile.name)
+                    if (index < row.size - 1) {
+                        append(",")
+                    }
+                }
+                append("]\n")
+            }
+        }
+    }
+
     fun createLevel(): Level {
 
         val reserveData = """
             |[E,C,B,A,D]
             |[D,A,E,C,B]
             """.trimMargin()
-
-        val levelData = """
-            |[D,B,A,B,D]
-            |[B,E,C,A,B]
-            |[B,E,C,C,B]
-            |[C,C,E,C,D]
-            |[B,B,C,A,D]
-            |[A,C,B,E,E]
-            |[D,E,E,B,A]
-            |[E,D,A,E,C]
-            |[E,A,D,C,B]
-            """.trimMargin()
+        val board = generateValidRandomBoard()
+        val levelData = boardToString(board)
 
         return Level(
             levelData, reserveData, maxMoves = 42,
